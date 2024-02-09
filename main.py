818f6d4b5ccee02c189dca4430bdeca15eb531dc
JuliaAndreyeva <blackonblackua@gmail.com>
2024-02-09 21:59:57 +0200

    added moving backward

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ FPS = 60
 def move_player(player_car):
     keys = pygame.key.get_pressed()
     moved = False
+
     if keys[pygame.K_a]:
         player_car.rotate(left=True)
     if keys[pygame.K_d]:
@@ -31,6 +32,10 @@ def move_player(player_car):
     if keys[pygame.K_w]:
         moved = True
         player_car.move_forward()
+    if keys[pygame.K_s]:
+        moved = True
+        player_car.move_backward()
+
     if not moved:
         player_car.reduce_speed()
 
