5afc37d11a54c37ad5ecf10d55c7f2a673aeeae6
JuliaAndreyeva <blackonblackua@gmail.com>
2024-02-09 21:56:06 +0200

    added keys menu, reduce speed, bounce

--- abstract_car.py
+++ abstract_car.py
@@ -58,3 +58,11 @@ class AbstractCar(ABC):
 class PlayerCar(AbstractCar):
     IMG = CAR
     START_POS = (180, 200)
+
+    def reduce_speed(self):
+        self.vel = max(self.vel - self.acceleration / 2, 0)
+        self.move()
+
+    def bounce(self):
+        self.vel = -self.vel
+        self.move()
--- main.py
+++ main.py
@@ -3,8 +3,15 @@ from utils import *
 from abstract_car import *
 
 GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
+
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
+TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
+
+FINISH = pygame.image.load("imgs/finish.png")
+FINISH_MASK = pygame.mask.from_surface(FINISH)
+FINISH_POSITION = (130, 250)
+
 CAR = scale_image(pygame.image.load("imgs/convertible.png"), 0.5)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
@@ -13,9 +20,26 @@ pygame.display.set_caption("CAR RACING!")
 
 FPS = 60
 
+
+def move_player(player_car):
+    keys = pygame.key.get_pressed()
+    moved = False
+    if keys[pygame.K_a]:
+        player_car.rotate(left=True)
+    if keys[pygame.K_d]:
+        player_car.rotate(right=True)
+    if keys[pygame.K_w]:
+        moved = True
+        player_car.move_forward()
+    if not moved:
+        player_car.reduce_speed()
+
+
 run = True
 clock = pygame.time.Clock()
-images = [(GRASS, (0, 0)), (TRACK, (0, 0))]
+images = [(GRASS, (0, 0)), (TRACK, (0, 0)),
+          (FINISH, FINISH_POSITION), (TRACK_BORDER, (0, 0))]
+
 player_car = PlayerCar(4, 4)
 
 while run:
@@ -28,19 +52,17 @@ while run:
             run = False
             break
 
-    keys = pygame.key.get_pressed()
-    moved = False
+    move_player(player_car)
 
-    if keys[pygame.K_a]:
-        player_car.rotate(left=True)
-    if keys[pygame.K_d]:
-        player_car.rotate(right=True)
-    if keys[pygame.K_w]:
-        moved = True
-        player_car.move_forward()
+    if player_car.collide(TRACK_BORDER_MASK) != None:
+        player_car.bounce()
 
-    if not moved:
-        player_car.reduce_speed()
+    finish_poi_collide = player_car.collide(FINISH_MASK, *FINISH_POSITION)
+    if finish_poi_collide != None:
+        if finish_poi_collide[1] == 0:
+            player_car.bounce()
+        else:
+            player_car.reset()
+            print("finish")
 
 pygame.quit()
-
