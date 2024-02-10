9af02be1ecb9cac6068b50092bb6703d2356d9de
Дарія Кравчук <dasha@MacBook-Pro-Daria.local>
2024-02-10 20:58:55 +0200

    added image

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ GRASS = scale_image(pygame.image.load("imgs/grass.jpg"), 2.5)
 
 TRACK = scale_image(pygame.image.load("imgs/track.png"), 0.9)
 TRACK_BORDER = scale_image(pygame.image.load("imgs/track-border.png"), 0.9)
+TRACK_BORDER2 = scale_image(pygame.image.load("imgs/track-border2.png"), 0.9)
 TRACK_BORDER_MASK = pygame.mask.from_surface(TRACK_BORDER)
 
 FINISH = pygame.image.load("imgs/finish.png")
